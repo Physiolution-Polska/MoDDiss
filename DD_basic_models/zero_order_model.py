df2100377f985057d36c75997574ce93af75f4b5
Eduard Maievskyi <30409199+EdwardMaievskyi@users.noreply.github.com>
2017-08-09 02:38:44 +0200

    Add files via upload

--- DD_basic_models/zero_order_model.py
+++ DD_basic_models/zero_order_model.py
@@ -5,36 +5,36 @@ Created on Sat Aug  5 14:19:29 2017
 @author: gbanach
 """
 
-def zero_order(t, k_0):
+def C_zero_order(k_0, t):
     """
     Gurny R, Doelker E, Peppas NA. Modelling of sustained release
     of water-soluble drugs from porous, hydrophobic polymers. Biomaterials. 1982;3:27–32.
 
     bibtexkey: gurny1982
     """    
-    F = k_0*t
+    Theor_C_zero_order = k_0*t
     
-    return F
+    return Theor_C_zero_order
 
 
-def zero_order_T(t, k_0, T_lag):
+def C_zero_order_T_lag(k_0, T_lag, t):
     """
     Borodkin S, Tucker FE. Linear drug release from laminated
     hydroxypropyl cellulose-polyvinyl acetate films. J Pharm Sci.1975;64:1289–94.
 
     bibtexkey: borodkin1975
     """   
-    F = k_0*(t - T_lag)
+    Theor_C_zero_order_T_lag = k_0*(t - T_lag)
     
-    return F
+    return Theor_C_zero_order_T_lag
 
 
-def zero_order_F0(t, k_0, F_0):
+def C_zero_order_F0( k_0, F_0, t):
     """
     Costa P, Sousa Lobo JM. Modeling and comparison of dissolution profiles. Eur J Pharm Sci. 2001;13:123–33.
 
     bibtexkey: costa2001
     """    
-    F = F_0 + k_0*t
+    Theor_C_zero_order_F_0 = F_0 + k_0*t
     
-    return F
+    return Theor_C_zero_order_F_0
