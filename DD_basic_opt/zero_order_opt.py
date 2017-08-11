35b970879b2df4459274947463b2b6981359aa47
Eduard Maievskyi <30409199+EdwardMaievskyi@users.noreply.github.com>
2017-08-11 03:31:19 +0200

    Add files via upload

--- DD_basic_opt/zero_order_opt.py
+++ DD_basic_opt/zero_order_opt.py
@@ -10,7 +10,14 @@ from scipy.optimize import differential_evolution
 
 def Find_PAR_DEv_zero_order (Texp, Cexp, k_0min=0., k_0max=100.):
     
-    """A differential evolution algorithm ( https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html )
+    """
+    Texp - an 1-D np.array of experimental data corresponding to the time elapsed from the beginning of the experiment.
+    Cexp - an 1-D np.array of experimental data corresponding to the drug concentration in the fixed moments defined by Texp.
+    k_0 - the optimal zero order model parameter
+    k_0min - an estimated minimal value of the k_0 parameter, which defines a lower boundary for the DEv algorithm
+    k_0max - an estimated maximal value of the k_0 parameter, which defines an upper boundary for the DEv algorithm
+       
+       A differential evolution algorithm ( https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html )
     is used to find an optimal parameter
     for zero order model by the minimazation of residual sum of squares RSS of experimentally estimated 
     and theoretically calculated values of drug concentration.
@@ -34,9 +41,18 @@ def Find_PAR_DEv_zero_order (Texp, Cexp, k_0min=0., k_0max=100.):
     PAR_zero_order = {'k_0': DEv_result_ZO.x[0]}
     return PAR_zero_order
 
-def Find_PAR_DEv_zero_order_T_lag (Texp, Cexp, k_0min=0., k_0max=10., T_lag_min = -10. , T_lag_max = 10.):
+def Find_PAR_DEv_zero_order_T_lag (Texp, Cexp, k_0min=0., k_0max=10., T_lag_min = -10. , T_lag_max = 100.):
     
-    """A differential evolution algorithm ( https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html )
+    """
+    Texp - an 1-D np.array of experimental data corresponding to the time elapsed from the beginning of the experiment.
+    Cexp - an 1-D np.array of experimental data corresponding to the drug concentration in the fixed moments defined by Texp.
+    k_0, T_lag - the optimal parameters for the first order model with Tlag 
+    k_0min - an estimated minimal value of the k_0 parameter, which defines a boundary for the DEv algorithm
+    k_0max - an estimated maximal value of the k_0 parameter, which defines a boundary for the DEv algorithm
+    T_lag_min - an estimated minimal value of the T_lag parameter, which defines a boundary for the DEv algorithm
+    T_lag_max - an estimated maximal value of the T_lag parameter, which defines a boundary for the DEv algorithm
+       
+       A differential evolution algorithm ( https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html )
     is used to find an optimal parameter
     for zero order model by the minimazation of residual sum of squares RSS of experimentally estimated 
     and theoretically calculated values of drug concentration.
@@ -61,3 +77,37 @@ def Find_PAR_DEv_zero_order_T_lag (Texp, Cexp, k_0min=0., k_0max=10., T_lag_min 
     PAR_zero_order_T_lag = {'k_0': DEv_result_ZO_T_lag.x[0], 'T_lag': DEv_result_ZO_T_lag.x[1]}
     return PAR_zero_order_T_lag
     
+def Find_PAR_DEv_zero_order_F0 (Texp, Cexp, k_0min=0., k_0max=10.,  F0_min=0., F0_max=100):
+    
+    """
+    Texp - an 1-D np.array of experimental data corresponding to the time elapsed from the beginning of the experiment.
+    Cexp - an 1-D np.array of experimental data corresponding to the drug concentration in the fixed moments defined by Texp.
+    k_0, T_lag - the optimal parameters for the first order model with Tlag 
+    k_0min - an estimated minimal value of the k_0 parameter, which defines a boundary for the DEv algorithm
+    k_0max - an estimated maximal value of the k_0 parameter, which defines a boundary for the DEv algorithm
+    F0_min - an estimated minimal value of the T_lag parameter, which defines a boundary for the DEv algorithm
+    F0_max - an estimated maximal value of the T_lag parameter, which defines a boundary for the DEv algorithm
+       
+       A differential evolution algorithm ( https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.differential_evolution.html )
+    is used to find an optimal parameter
+    for zero order model by the minimazation of residual sum of squares RSS of experimentally estimated 
+    and theoretically calculated values of drug concentration.
+    
+    Reference to zero order with time lag model:
+    Costa P, Sousa Lobo JM. Modeling and comparison of dissolution profiles. Eur J Pharm Sci. 2001;13:123–33.
+
+    bibtexkey: costa2001"""
+    
+    k_0_F0_bounds = [(k_0min, k_0max) , (F0_min, F0_max)]
+    
+    
+    def RSS_ZO_F0 (X_ZOF0):
+        
+        Theor_C_zero_order_F0 = X_ZOF0[0] * Texp + X_ZOF0[1]
+        RSSzoF0= sum((Cexp-Theor_C_zero_order_F0)**2)
+        return RSSzoF0
+    
+    
+    DEv_result_ZO_F0 = differential_evolution(RSS_ZO_F0, bounds = k_0_F0_bounds , maxiter=100000000, strategy='best1bin', popsize=50, mutation=(0.9, 1.8))
+    PAR_zero_order_F0 = {'k_0': DEv_result_ZO_F0.x[0], 'F0':DEv_result_ZO_F0.x[1]}
+    return PAR_zero_order_F0
