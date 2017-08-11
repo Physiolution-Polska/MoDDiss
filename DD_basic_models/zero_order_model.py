d7f5059fe915483ce7ab29fb81df6b8e6f2e543f
Eduard Maievskyi <30409199+EdwardMaievskyi@users.noreply.github.com>
2017-08-11 03:30:43 +0200

    Add files via upload
    corrected description and functionality

--- DD_basic_models/zero_order_model.py
+++ DD_basic_models/zero_order_model.py
@@ -7,6 +7,9 @@ Created on Sat Aug  5 14:19:29 2017
 
 def C_zero_order(k_0, t):
     """
+    k_0- parameter of the model
+    t - an 1-D np.array of data corresponding to the time elapsed from the beginning of the experiment
+    
     Gurny R, Doelker E, Peppas NA. Modelling of sustained release
     of water-soluble drugs from porous, hydrophobic polymers. Biomaterials. 1982;3:27–32.
 
@@ -19,6 +22,9 @@ def C_zero_order(k_0, t):
 
 def C_zero_order_T_lag(k_0, T_lag, t):
     """
+    k_0, T_lag- parameters of the model
+    t - an 1-D np.array of data corresponding to the time elapsed from the beginning of the experiment
+    
     Borodkin S, Tucker FE. Linear drug release from laminated
     hydroxypropyl cellulose-polyvinyl acetate films. J Pharm Sci.1975;64:1289–94.
 
@@ -31,6 +37,8 @@ def C_zero_order_T_lag(k_0, T_lag, t):
 
 def C_zero_order_F0( k_0, F_0, t):
     """
+    k_0, F_0- parameters of the model
+    t - an 1-D np.array of data corresponding to the time elapsed from the beginning of the experiment
     Costa P, Sousa Lobo JM. Modeling and comparison of dissolution profiles. Eur J Pharm Sci. 2001;13:123–33.
 
     bibtexkey: costa2001
